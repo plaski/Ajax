f1ae7eefa00ccd9aa7eb18f7b302db8b23936699
plaski <maciejploskonka@gmail.com>
2017-03-18 13:59:51 +0100

    dziala, ale slabo wyglada

--- scripts.js
+++ scripts.js
@@ -1,22 +1,32 @@
-var url = 'http://api.icndb.com/jokes/random';
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function () {
-	getJoke();
+function getQuote() {
+	$.getJSON(quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+	if (!input.quoteAuthor.length) {
+		input.quoteAuthor = "Autor nieznany";
+	}
+
+	var tweetText = "Cytat na dziś to: " + input.quoteText + " autorstwa: " +input.quoteAuthor;
+
+	if (tweetText.length > 140) {
+		getQuote();
+	} else {
+		var tweet = tweetLink + encodeURIComponent(tweetText);
+		$('.quote').text(input.quoteText);
+		$('.author').text("Author: " + input.quoteAuthor);
+		$('.tweet').attr('href', tweet);
+	}
+
+	$('.tweet').attr('href', tweet);
+}
+
+$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
+	})
 });
-
-var paragraph = document.getElementById('joke');
-
-window.onload = function() {
-	getJoke();
-}
-
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-  	xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerText = response.value.joke;
-  });
-  xhr.send();
-}
