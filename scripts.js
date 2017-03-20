ad29b43454ef27f574f7caaa2bc40f479da3f9c5
plaski <maciejploskonka@gmail.com>
2017-03-20 20:12:02 +0100

    kraje dziala

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-var url = 'http://api.icndb.com/jokes/random';
+var url = 'https://restcountries.eu/rest/v2/name/';
+var countriesList = $('#countries');
 
-var button = document.getElementById('get-joke');
-button.addEventListener('click', function () {
-	getJoke();
-});
+$('#search').click(searchCountries);
 
-var paragraph = document.getElementById('joke');
+function searchCountries() {
+	var countryName = $('#country-name').val();
 
-window.onload = function() {
-	getJoke();
+	if(!countryName.length) countryName = 'Poland';
+	$.ajax({
+		url: url + countryName,
+		method: 'GET',
+		success: showCountriesList
+	});
 }
 
-function getJoke() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-  	xhr.addEventListener('load', function(){
-    var response = JSON.parse(xhr.response);
-    paragraph.innerText = response.value.joke;
-  });
-  xhr.send();
+function showCountriesList(resp) {
+	countriesList.empty();
+	resp.forEach(function(item) {
+		$('<li>').text(item.name).appendTo(countriesList);
+	});
 }
