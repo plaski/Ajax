4b7eaba50e69b28f9f6d9ed62cd9d23c44cafb91
plaski <maciejploskonka@gmail.com>
2017-03-26 15:27:54 +0200

    dziala

---
+++ App.js
@@ -0,0 +1,32 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '1621',
+  'X-Auth-Token': 'd9b11d12ace12970cd6894df2db6e33c'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+};
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	})
+};
